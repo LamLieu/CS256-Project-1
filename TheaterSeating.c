02889ed608762476701828a533f68ee7fe774798
LamLieu <lieulam@hotmail.com>
2018-04-23 19:44:04 -0700

    Added the finishing touches

--- TheaterSeating.c
+++ TheaterSeating.c
@@ -8,7 +8,7 @@ void displaySeats(char *seats) {
 
 	for (int rowNum = 0; rowNum < 15; rowNum++) {
 		printf("Row %d\t", rowNum + 1);
-		for (int seatNum =  0; seatNum < 30; seatNum++) {
+		for (int seatNum = 0; seatNum < 30; seatNum++) {
 			printf("%c", *((seats + rowNum * 30) + seatNum));
 		}
 		printf("\n");
@@ -19,8 +19,6 @@ int main() {
 	double priceOfRow[15];
 	double totalTicketSales = 0;
 	int seatsSold = 0;
-
-	int flag = 0; //0 for false, 1 for true
 
 	for (int row = 0; row < 15; row++) {
 		printf("Please enter the price of row %d: ", row + 1);
@@ -37,63 +35,75 @@ int main() {
 		}
 	}
 
-	while (flag == 0) {
-		displaySeats((char *)seats);
-		int row, seat;
+	displaySeats((char *) seats); //Displays
 
-		do {
+	int row, seat;
+
+	do {
 		printf("Please enter the row that you want (1-15): ");
 		fflush(stdout);
 		scanf("%d", &row);
-		} while (row < 1 || row > 15);
+	} while (row < 1 || row > 15);
 
-		do {
-		printf("Please enter the seat that you want in row %d (1-15): ", row);
+	do {
+		printf("Please enter the seat that you want in row %d (1-30): ", row);
 		fflush(stdout);
 		scanf("%d", &seat);
-		} while (seat < 1 || seat > 30);
+	} while (seat < 1 || seat > 30);
 
-		if (seats[row][seat] == '*') {
-			seats[row][seat] = '#';
-			totalTicketSales += priceOfRow[row];
-			seatsSold++;
-		}
-		else {
-			printf("Row #%d, Seat #%d is taken.\n", row, seat);
-		}
+	if (seats[row][seat] == '*') {
+		seats[row][seat] = '#';
+		totalTicketSales += priceOfRow[row];
+		seatsSold++;
+	} else {
+		printf("Row #%d, Seat #%d is taken.\n\n", row, seat);
+	}
 
-		int choice = 0;
-		do {
+	int choice = 0;
+
+	do {
 		printf("Additional Options\n");
-		printf("-------------------------");
+		printf("-------------------------\n");
 		printf("(1) View total number of ticket sales\n");
 		printf("(2) View number of seats sold\n");
 		printf("(3) View available seats in a row\n");
-		printf("(4) View total number of available seats\n");
+		printf("(4) View total number of available seats in the auditorium\n");
+		printf("(5) Exit program\n-->");
 		fflush(stdout);
-		scanf("%c", &choice);
-		} while (choice < 1 || choice > 4);
+		scanf("%d", &choice);
+	} while (choice < 1 || choice > 4);
 
-		switch(choice) {
-		case 1:
-			printf("The total number of all ticket sales is: %.2lf", totalTicketSales);
-			break;
-		case 2:
-			printf("The number of seats sold is: %d", seatsSold);
-			break;
-		case 3:
-			printf("The number of seats available in each row are: %d", seatsSold);
-			break;
-		case 4:
-			printf("The total number of seats available in the auditorium is:", 30 - seatsSold);
-			break;
+	int rowNumber;
+	int numOfSeatsInRow = 0;
 
+	switch (choice) {
+	case 1:
+		printf("The total number of all ticket sales is: $%.2lf",
+				totalTicketSales);
+		break;
+	case 2:
+		printf("The number of seats sold is: %d", seatsSold);
+		break;
+	case 3:
+		do {
+			printf("Please enter the row that you want to check (1-15): ");
+			fflush(stdout);
+			scanf("%d", &rowNumber);
+		} while (rowNumber < 1 || rowNumber > 15);
 
+		for (int seatNumber = 0; seatNumber < 30; seatNumber++) {
+			if (seats[row][seatNumber] == '*') {
+				numOfSeatsInRow++;
+			}
+		}
+		printf("The number of seats available in row %d is: %d", rowNumber,
+				numOfSeatsInRow);
+		break;
+	case 4:
+		printf("The total number of seats available in the auditorium is: %d",
+				30 - seatsSold);
+		break;
 
+		return 0;
 	}
-
-
-
-
-	return 0;
 }
